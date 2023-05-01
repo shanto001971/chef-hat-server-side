1457ebb2e198f01e06b7e764c36535287a2fbf22
Shanto Islam <rajs45693@gmail.com>
2023-05-02 00:00:29 +0600

    create json data

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const chefs = require('./chef.json')
+const cors = require('cors');
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send(chefs)
+})
+app.get('/data/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const chef = chefs.find(singleChef => singleChef.id === id) || {};
+    res.send(chef)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
